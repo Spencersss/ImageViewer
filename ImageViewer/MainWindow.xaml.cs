12d862c0d6d6b1d0c4809767d2c7113ac0550b51
Spencer Stissi <sas7359@rit.edu>
2021-11-01 10:18:29 -0400

    Init

---
+++ ImageViewer/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WebImage Image;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateDetailsPanel(WebImage image)
+        {
+            string imageTitle = image.GetImageTitle();
+            if (imageTitle.Length > 0)
+            {
+                image_type.Content = $"FILETYPE: {imageTitle.Substring(imageTitle.Length - 4)}";
+            } 
+            else
+            {
+                image_type.Content = $"FILETYPE:";
+            }
+            image_title.Content = $"TITLE: {imageTitle.Substring(0, imageTitle.Length - 4)}";
+            image_width.Content = $"WIDTH: {image.GetImageWidth()}px";
+            image_height.Content = $"HEIGHT: {image.GetImageHeight()}px";
+        }
+
+        private async Task<WebImage> RequestImage(string uri)
+        {
+            WebImage newImage = new WebImage(uri);
+            try
+            {
+                error_label.Visibility = Visibility.Hidden;
+                bool loaded = await newImage.LoadImage();
+                if (loaded)
+                {
+                    image_preview.Source = newImage.GetImage();
+                }
+                else
+                {
+                    error_label.Visibility = Visibility.Visible;
+                }
+                return newImage;
+            }
+            catch (Exception e)
+            {
+                error_label.Visibility = Visibility.Visible;
+                return null;
+            }
+        }
+
+        private async void Submit_Preview(object sender, RoutedEventArgs e)
+        {
+            Image = await RequestImage(image_url.Text);
+            if (Image != null)
+            {
+                Image.GetImage().DownloadCompleted += (s, evtArgs) =>
+                {
+                    UpdateDetailsPanel(Image);
+                };
+            }
+        }
+
+        private void Download_Image(object sender, RoutedEventArgs e)
+        {
+            if (Image != null && !Image.GetImage().IsDownloading)
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.FileName = Image.GetImageTitle().Substring(Image.GetImageTitle().Length - 4);
+                dialog.DefaultExt = ".png";
+                dialog.Filter = "Picture (.png)|*.png";
+                //TODO: Support more formats other than PNG.
+
+                bool? dialogOpened = dialog.ShowDialog();
+
+                if (dialogOpened == true)
+                {
+                    // BitmapImage saving source: https://stackoverflow.com/questions/35804375/how-do-i-save-a-bitmapimage-from-memory-into-a-file-in-wpf-c 
+                    BitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(Image.GetImage()));
+                    Stream imageStream = new FileStream(dialog.FileName, FileMode.Create);
+                    encoder.Save(imageStream);
+                }
+            }
+        }
+
+    }
+}
---
+++ ImageViewer/WebImage.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ImageViewer
+{
+    class WebImage
+    {
+        // Data
+        private string s_uri;
+        private BitmapImage s_image;
+        private static HttpClient Client = new HttpClient();
+
+        // Constructors
+        public WebImage(string imageUri)
+        {
+            s_uri = imageUri;
+        }
+
+        // Methods
+        public BitmapImage GetImage()
+        {
+            return s_image;
+        }
+
+        public string GetImageTitle()
+        {
+            // Regex Source: https://stackoverflow.com/questions/37807568/get-only-the-image-filename-from-a-url-with-regex
+            Regex regex = new Regex(@"[\w-]+\.(jpg|png|gif|PNG|JPG|GIF)");
+            return regex.Match(s_uri).Value;
+        }
+
+        public int GetImageHeight()
+        {
+            if (s_image != null)
+            {
+                return s_image.PixelHeight;
+            }
+            return 0;
+        }
+
+        public int GetImageWidth()
+        {
+            if (s_image != null)
+            {
+                return s_image.PixelWidth;
+            }
+            return 0;
+        }
+        
+        /*
+         * Makes a GET request to a provided uri leading to an image.
+         * If successful returns true, else returns false.
+         */
+        public async Task<bool> LoadImage()
+        {
+            try
+            {
+                Uri imgUri = new Uri(s_uri);
+                HttpResponseMessage res = await Client.GetAsync(s_uri);
+                if (res.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    s_image = new BitmapImage(imgUri);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+}
